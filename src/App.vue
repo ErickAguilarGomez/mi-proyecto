c2034f85d15c274ba83bf23636eaa37b2b7abec5
ErickAguilarGomez <ErickAguilarGomez>
2025-01-08 18:56:20 -0500

    hola

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,156 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <div class="w-100 d-flex justify-center flex-column">
+
+      <b-button v-b-modal.modal-prevent-closing class="bg-primary mx-auto">Add New Client</b-button>
+
+      <b-modal id="modal-prevent-closing" ref="modal" title="Submit Client Info" @show="resetModal" @hidden="resetModal"
+        @ok="handleOk">
+
+        <form ref="form" @submit.stop.prevent="handleSubmit">
+          <b-form-group label="Name" label-for="name-input" invalid-feedback="Name is required" :state="nameState">
+            <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Email" label-for="email-input" invalid-feedback="Email is required" :state="emailState">
+            <b-form-input id="email-input" v-model="email" :state="emailState" required></b-form-input>
+          </b-form-group>
+
+          <b-form-group label="Phone" label-for="phone-input" invalid-feedback="phone is required" :state="phoneState">
+            <b-form-input id="phone-input" v-model="phone" :state="phoneState" required></b-form-input>
+          </b-form-group>
+        </form>
+
+      </b-modal>
+
+    </div>
+
+    <table cellpadding="10" cellspacing="0" class="w-100">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>EMAIL</th>
+          <th>PHONE</th>
+          <th>TRANSACTIONS</th>
+          <th>OPTIONS</th>
+        </tr>
+      </thead>
+
+      <tbody v-if="clients.length != 0">
+        <tr v-for="client in clients" :key="client.id" class="border border-primary">
+          <td>{{ client.id }}</td>
+          <td>{{ client.name }}</td>
+          <td>{{ client.email }}</td>
+          <td>{{ client.phone }}</td>
+          <td v-for="clientTransaction in client.transtactions"  class="d-flex flex-col gap-5">
+            <div class="border border-primary  p-1" >{{ clientTransaction.amount }} </div>
+            <div class="border border-primary  p-1">{{ clientTransaction.date }}</div>
+          </td>
+          <td class="d-flex gap-3">
+            <b-button variant="danger" @click="deleteClient(client.id)">Delete</b-button>
+            <HelloWorld :dataClient="client"/>
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      name: '',
+      email: '',
+      phone: '',
+      clients: [],
+      nameState: null,
+      emailState: null,
+      phoneState: null
+    }
+  },
+  mounted() {
+    this.fetchClients();
+  },
+
+  watch: {
+    'clients.length': function (newLength, oldLength) {
+      this.fetchClients();
+    }
+  },
+
+  methods: {
+    async fetchClients() {
+      const reponse = await axios.get("http://127.0.0.1:8000/home")
+      this.clients = reponse.data.data
+      console.log(this.clients)
+    },
+
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity()
+      this.nameState = valid
+      this.emailState = valid
+      this.phoneState = valid
+      return valid
+    },
+    resetModal() {
+      this.name = '';
+      this.email = '';
+      this.phone = '';
+      this.nameState = null;
+      this.emailState = null;
+      this.phoneState = null;
+    },
+    handleOk(bvModalEvent) {
+      bvModalEvent.preventDefault()
+      this.handleSubmit()
+    },
+    async handleSubmit() {
+      if (!this.checkFormValidity()) {
+        return
+      }
+      this.clients.push(this.name)
+      await this.createClient()
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-prevent-closing')
+      })
+
+    },
+
+    async createClient() {
+      const { data } = await axios.post("http://127.0.0.1:8000/create", {
+        name: this.name,
+        email: this.email,
+        phone: this.phone
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      )
+    },
+
+    async deleteClient(id) {
+      const { data } = await axios.delete(`http://127.0.0.1:8000/delete/${id}`)
+      this.clients = this.clients.filter((client) => client.id != id)
+      console.log(data.data)
+    }
+
+
   }
 }
 </script>
+
 
 <style>
 #app {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,189 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!-- Botón para abrir el modal -->
+    <b-button @click="showModal">Update</b-button>
+
+    <!-- Modal con ID dinámico -->
+    <b-modal :id="`modal-client-${dataClient.id}`" ref="modal" :title="`Update Client: ${dataClient.name}`"
+      @show="resetModal" @hidden="resetModal" @ok="handleOk">
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <!-- Campos del formulario -->
+
+        <b-form-group label="Name" label-for="name-input" invalid-feedback="Name is required" :state="nameState">
+          <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Email" label-for="email-input" invalid-feedback="Email is required" :state="emailState">
+          <b-form-input id="email-input" v-model="email" :state="emailState" type="email" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Phone" label-for="phone-input" invalid-feedback="Phone is required" :state="phoneState">
+          <b-form-input id="phone-input" v-model="phone" :state="phoneState" required></b-form-input>
+        </b-form-group>
+
+        <h6>Transactions</h6>
+
+        <b-form-group v-for="(transaction, index) in transtactionsPerClient" :key="transaction.id"
+          class="flex flex-col md:flex-row gap-4 p-4 rounded-lg bg-gray-100 shadow-lg">
+          <b-form-input type="number" v-model="transaction.amount"
+            class="w-full md:w-2/3 p-3 text-lg border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400"
+            placeholder="Amount" :state="transaction.amount ? true : false"></b-form-input>
+
+          <b-form-input type="date" v-model="transaction.date"
+            class="w-full md:w-1/3 p-3 text-lg border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400"
+            placeholder="Date" :state="transaction.date ? true : false"></b-form-input>
+
+          <!-- Botón para eliminar una transacción específica -->
+          <b-button variant="danger" @click="deleteTransaction(index)">Delete Transaction</b-button>
+        </b-form-group>
+
+        <!-- Botón para añadir más transacciones -->
+        <b-button variant="success" @click="addTransaction">Add Transaction</b-button>
+
+      </form>
+    </b-modal>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  props: {
+    dataClient: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      id: this.dataClient.id,
+      name: this.dataClient.name || "",
+      email: this.dataClient.email || "",
+      phone: this.dataClient.phone || "",
+      nameState: null,
+      emailState: null,
+      phoneState: null,
+      transtactionsPerClient: this.dataClient.transtactions || []
+    };
+  },
+  methods: {
+    // Abre el modal dinámicamente
+    showModal() {
+      this.$bvModal.show(`modal-client-${this.dataClient.id}`);
+
+      // Si no hay transacciones iniciales, crea una por defecto
+      if (this.transtactionsPerClient.length === 0) {
+        this.addTransaction();
+      }
+
+      console.log("Hola desde update", JSON.stringify(this.transtactionsPerClient));
+    },
+
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid;
+      this.emailState = valid;
+      this.phoneState = valid;
+      return valid;
+    },
+
+    resetModal() {
+      this.name = this.dataClient.name || "";
+      this.email = this.dataClient.email || "";
+      this.phone = this.dataClient.phone || "";
+      this.nameState = null;
+      this.emailState = null;
+      this.phoneState = null;
+    },
+
+    // Se llama cuando se presiona OK en el modal
+    handleOk(bvModalEvent) {
+      bvModalEvent.preventDefault();
+      this.handleSubmit();
+    },
+
+    handleSubmit() {
+      if (!this.checkFormValidity()) {
+        return;
+      }
+
+      if (!this.validateTransactions()) {
+        return; // Si las transacciones no son válidas, no continuar con el envío
+      }
+
+      this.$nextTick(() => {
+        this.$bvModal.hide(`modal-client-${this.dataClient.id}`);
+        console.log("Form Submitted!");
+        this.updateClient(); // Llamada a la función de actualización
+      });
+    },
+
+    async updateClient() {
+      try {
+        // Actualizar los datos del cliente
+        const bodyData = {
+          id: this.dataClient.id,
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+        };
+
+        const { data } = await axios.patch("http://127.0.0.1:8000/update", bodyData, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        });
+
+        await this.inserTransactions()
+
+      } catch (error) {
+        console.error('Error al actualizar el cliente o las transacciones:', error.data || error.message);
+      }
+    },
+
+    async inserTransactions() {
+      try {
+        const { data } = await axios.post("http://127.0.0.1:8000/insert/transactions", JSON.stringify(this.transtactionsPerClient), {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        });
+        console.log('transacciones insertadas: ', data);
+
+      } catch (error) {
+        console.error('Error al actualizar el cliente o las transacciones:', error.data || error.message);
+      }
+    },
+
+    validateTransactions() {
+      for (let transaction of this.transtactionsPerClient) {
+        if (!transaction.amount || !transaction.date) {
+          alert('Please fill in all transaction fields.');
+          return false;
+        }
+      }
+      return true;
+    },
+
+    addTransaction() {
+      if (this.transtactionsPerClient.some(transaction => !transaction.amount || !transaction.date)) {
+        alert("Please fill in all transaction fields before adding a new one.");
+        return;
+      }
+
+      const newTransaction = {
+        clientID: this.dataClient.id,
+        amount: null,
+        date: ""
+      };
+
+      this.transtactionsPerClient.push(newTransaction);
+    },
+
+    deleteTransaction(index) {
+      this.transtactionsPerClient.splice(index, 1);
+    }
+  }
+};
+</script>
